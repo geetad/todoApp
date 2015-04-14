722274ad273ec539c45f6d181e9a127762c64584
geetad <geetadesaraju@gmail.com>
2015-04-14 08:23:22 -0700

    Added files to gitHub

---
+++ app/src/main/java/com/astro/todoapp/EditItemActivity.java
@@ -0,0 +1,85 @@
+package com.astro.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class EditItemActivity extends ActionBarActivity {
+
+    String passedItem = null;
+    private EditText tvMultiline = null;
+    private Button btnSave = null;
+    public final static String item_Changed= "com.astro.todoapp._ID";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_item);
+
+        passedItem = getIntent().getStringExtra(TodoListActivity.Id_Extra);
+        tvMultiline = (EditText)findViewById(R.id.editLstItem);
+        tvMultiline.setText(passedItem);
+
+        tvMultiline.setFocusableInTouchMode(true);
+        tvMultiline.requestFocus();
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+    }
+
+    public void updateItem(View v)
+    {
+        tvMultiline.setEnabled(true);
+        tvMultiline.setFocusable(true);
+    }
+
+    public void btnOnClick(View v)
+    {
+        String ret = tvMultiline.getText().toString();
+        Toast.makeText(this, ret, Toast.LENGTH_SHORT).show();
+
+         Intent i = new Intent();
+         i.putExtra("item_Changed",tvMultiline.getText().toString());
+         setResult(RESULT_OK,i);
+
+        finish();
+    }
+
+
+    public void onBackPressed()
+    {
+        //string data = tvMultiline.getText().toString();
+       // Intent i = new Intent();
+        //i.putExtra("item_Changed",tvMultiline.getText().toString());
+        //setResult(RESULT_OK,i);
+        //finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_edit_item, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/astro/todoapp/TodoListActivity.java
@@ -0,0 +1,179 @@
+package com.astro.todoapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class TodoListActivity extends ActionBarActivity {
+
+    private ArrayList<String> todoItems;
+    public ArrayAdapter<String> todoAdapter;
+    private ListView lvItems;
+    private EditText etView;
+    private Boolean isEditable = false;
+    private int req_code=0;
+
+    public final static String Id_Extra= "com.astro.todoapp._ID";
+  //  public final static int REQ_CODE = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_todo_list);
+
+        etView = (EditText)findViewById(R.id.etView);
+        lvItems = (ListView)findViewById(R.id.lvItems);
+       //populateArrayItems();
+        readItems();
+        todoAdapter= new ArrayAdapter(this,android.R.layout.simple_list_item_1,todoItems);
+        lvItems.setAdapter(todoAdapter);
+
+        setupListViewListener();
+        lvItems.setOnItemClickListener(onListClick);
+    }
+
+    private void readItems()
+    {
+      File filesDir = getFilesDir();
+       File todoFile = new File(filesDir,"todo.txt");
+
+       try{
+           todoItems = new ArrayList<String>(FileUtils.readLines(todoFile));
+
+        }catch(java.io.IOException e)
+        {
+            todoItems=new ArrayList<>();
+            e.printStackTrace();
+        }
+    }
+
+    private void saveItems()
+    {
+        java.io.File filesDir = getFilesDir();
+        java.io.File todoFile= new java.io.File(filesDir,"todo.txt");
+        try
+        {
+            FileUtils.writeLines(todoFile, todoItems);
+
+            //Environment.getRootDirectory()
+             Toast.makeText(getApplicationContext(), Environment.getRootDirectory().toString(), Toast.LENGTH_SHORT).show();
+
+        }catch(java.io.IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener()
+    {
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+        {
+            // When clicked, show a toast with the TextView text
+            Intent myIntent = new Intent(view.getContext(), EditItemActivity.class);
+
+            myIntent.putExtra(Id_Extra, todoItems.get((int) id));
+            //startActivity(myIntent);
+            req_code=(int)id;
+            startActivityForResult(myIntent, req_code);
+
+            // Toast.makeText(getApplicationContext(),
+            // ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+        }
+
+    };
+
+    private void setupListViewListener()
+    {
+            lvItems.setOnItemLongClickListener(new OnItemLongClickListener()
+            {
+                public boolean onItemLongClick(AdapterView<?> adapter,View item,int pos,long id)
+                {
+                    todoItems.remove(pos);
+                    todoAdapter.notifyDataSetChanged();
+                    saveItems();
+                    return true;
+                }
+            });
+    }
+
+    private void populateArrayItems() {
+
+        todoItems = new ArrayList<String>();
+        todoItems.add("Buy Milk");
+        todoItems.add("Do Laundry");
+        todoItems.add("Get NewsPaper");
+        todoItems.add("Get Groceries");
+    }
+
+    private void makeEditable(Boolean isEditable)
+    {
+        etView.setFocusable(true);
+        etView.setEnabled(true);
+        etView.setClickable(true);
+        etView.setFocusableInTouchMode(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_todo_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        if(requestCode ==req_code){
+            if (resultCode == RESULT_OK)
+            {
+                String strReturned = data.getStringExtra("item_Changed");
+                Toast.makeText(this, strReturned,Toast.LENGTH_SHORT).show();
+
+                //setText(req_code);
+                todoAdapter.remove(todoAdapter.getItem(req_code));
+                todoAdapter.insert(strReturned, req_code);
+              // todoAdapter.add(strReturned);
+               todoAdapter.notifyDataSetChanged();
+               saveItems();
+            }
+        }
+    }
+
+    public void addTodoItem(View view) {
+
+        EditText txtNewItem = (EditText)findViewById(R.id.etView);
+        todoAdapter.add(txtNewItem.getText().toString());
+        saveItems();
+        etView.setText("");
+    }
+}
